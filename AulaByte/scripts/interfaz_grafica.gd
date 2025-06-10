c4b790fb0e6f132fc401834a390097708dd03d72
Chrissar <chraken19@gmail.com>
2025-06-10 04:08:13 -0500

    Recolección de monedas
    Se implemento el HUD (interfaz de usuario) y se crearon las monedas

---
+++ AulaByte/scripts/interfaz_grafica.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+@onready var lb_bits: Label = $lbBits
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	visible = true
+	var game_manager = get_node("%GameManager")
+	game_manager.puntuacion_actualizada.connect(_on_puntuacion_actualizada)
+
+func _on_puntuacion_actualizada(puntuacion_actual: int) -> void:
+	lb_bits.text = "X " + str(puntuacion_actual)
