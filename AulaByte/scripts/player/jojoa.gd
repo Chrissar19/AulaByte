4d7299069d1381128344fabc8e9ca2971a72a936
Chrissar <chraken19@gmail.com>
2025-07-09 02:28:06 -0500

    Maquina de estados simple
    Se mejoro y modifico la maquina de estados

---
+++ AulaByte/scripts/player/jojoa.gd
@@ -0,0 +1,6 @@
+extends PersonajeME
+
+func _ready():
+	nombre = "Jojoa"
+	equipo = "No"
+	super._ready()
--- AulaByte/scripts/states/player_states.gd
+++
@@ -1,8 +0,0 @@
-extends Resource
-class_name PlayerStates
-
-var _idle = "PlayerStateIdle"
-var _walk = "PlayerStateWalk"
-var _push = "PlayerStatePush"
-var _jump_up = "PlayerStateJumpUp"
-var _jump_down = "PlayerStateJumpDown"
