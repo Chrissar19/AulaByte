3685d056e0342e40c665a1f1c5da65d7585604c1
C0D199 <dianacortez9924@gmail.com>
2025-07-14 22:59:00 -0500

    Nivel 2 piso
    se desarrolla el piso del nivel 2

--- AulaByte/scripts/zona_muerta.gd
+++ AulaByte/scripts/zona_muerta.gd
@@ -6,7 +6,7 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void: #Activa la zona de muerte
 	sound_death.play() # Reproduce el sonido de muerte
 	print("Capturado")
-	Engine.time_scale = 0.5 #Controla la velocidad (tiempo) en el juego siendo 1 va vel normal
+	Engine.time_scale = 0.8 #Controla la velocidad (tiempo) en el juego siendo 1 va vel normal
 	body.get_node("CollisionShape2D").queue_free() #Elimina la colision del jugador para que pueda caer al vacio
 	timer.start() #Una vez activa la zona de muerte de activa el NodoTimer.
 
