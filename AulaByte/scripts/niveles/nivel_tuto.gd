4b79b18110ca25af32ebd9142f7f99991ec1c79d
Chrissar19 <chraken19@gmail.com>
2025-07-21 11:32:55 -0500

    Organizar carpetas
    Se organizo la el orden de las carpetas

--- AulaByte/scripts/niveles/nivel_tuto.gd
+++ AulaByte/scripts/niveles/nivel_tuto.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 @onready var contenedor: Node2D = self
+@onready var punto_inicio_jugador: Node2D = $PuntoInicioJugador
+@onready var puntos_inicio: Node2D = $PuntosInicio
 
 func _ready() -> void:
 	var id := JugadorSeleccionado.obtener_id()
@@ -9,7 +11,7 @@ func _ready() -> void:
 	if ruta != "":
 		var escena_personaje := load(ruta)
 		var jugador: Node2D = escena_personaje.instantiate()
-		jugador.position = Vector2(3, 0) #--Posicion de inicio
+		jugador.position = punto_inicio_jugador.position #--Posicion de inicio
 		contenedor.add_child(jugador)
 	else:
 		print("No se cargo el personaje")
