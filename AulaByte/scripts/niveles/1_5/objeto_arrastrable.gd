7c8e949ede709c0ae50f231db8081201973d5f69
Chrissar <chraken19@gmail.com>
2025-10-20 22:41:38 -0500

    Nivel 6

--- AulaByte/scripts/niveles/1_5/objeto_arrastrable.gd
+++ AulaByte/scripts/niveles/1_5/objeto_arrastrable.gd
@@ -8,6 +8,7 @@ signal return_requested
 var _start_position: Vector2
 
 func _ready() -> void:
+    
     mouse_filter = Control.MOUSE_FILTER_STOP
     _start_position = position
 
@@ -19,8 +20,15 @@ func _get_drag_data(_at_position: Vector2) -> Variant:
     }
     var preview := duplicate() as TextureRect
     preview.modulate.a = 0.6
+    
+    var visual_size: Vector2 = preview.size
+    var visual_scale: Vector2 = preview.scale
+        
+    var pivot_x: float = -(visual_scale.x / (2.0 * (1.0 - visual_scale.x))) * visual_size.x
+    var pivot_y: float = -(visual_scale.y / (2.0 * (1.0 - visual_scale.y))) * visual_size.y
+    preview.pivot_offset = Vector2(pivot_x, pivot_y)
+    
     set_drag_preview(preview)
-
     visible = false
     return data
 
