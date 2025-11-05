938963a77f9470bcc12c0b557eabaeed7403409b
Chrissar <chraken19@gmail.com>
2025-11-05 16:37:54 -0500

    Temporizador Interruotores
    Se creo un temporizador que ayuda a arreglar el tiempo de activacion de los interruptores...

--- AulaByte/scripts/niveles/6_10/interruptor.gd
+++ AulaByte/scripts/niveles/6_10/interruptor.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-signal activado(id)
+signal activado(id: String, ref: Node)
 
 @export_enum("A", "B", "C", "D", "E", "F", "G") var id: String = "A"
 @export_enum("azul", "amarilla", "roja", "morada", "verde") var color_inter: String = "verde"
@@ -8,6 +8,7 @@ signal activado(id)
 
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var t_feedback: Timer = $Timer
 
 var jugador_en_rango: bool = false
 
@@ -20,6 +21,13 @@ func _ready() -> void:
     if collision_shape_2d:
         collision_shape_2d.disabled = false
     asignar_color()
+    if anim.sprite_frames.has_animation("desactivado"):
+        anim.play("desactivado")
+    activo = false 
+    
+    t_feedback.one_shot = true
+    add_child(t_feedback)
+    t_feedback.timeout.connect(_on_timer_timeout)
 
 #-----------------------------------------------------------------------------------
 func _on_body_entered(body: Node) -> void:
@@ -33,8 +41,7 @@ func _on_body_exited(body: Node) -> void:
 #-----------------------------------------------------------------------------------
 func _process(_delta: float) -> void:
     if not activo and jugador_en_rango and Input.is_action_just_pressed("Accion"):
-        emit_signal("activado", id)
-        _animar_interruptor()
+        emit_signal("activado", id, self)
 
 #-----------------------------------------------------------------------------------
 func asignar_color() -> void:
@@ -46,7 +53,26 @@ func asignar_color() -> void:
         "verde":    anim.modulate = Color(0.5, 1, 0.5)
 
 #-----------------------------------------------------------------------------------
-func _animar_interruptor() -> void:
+func activar_visual() -> void:
     # Reproduce la animación solo una vez
-    anim.play("activado")
+    if anim.sprite_frames.has_animation("activado"):
+        anim.play("activado")
     activo = true
+    
+func desactivar_visual() -> void:
+    if anim.sprite_frames.has_animation("desactivado"):
+        anim.play("desactivado")
+    activo = false
+    
+    
+func presionar_temporal(seg: float = 3.0) -> void:
+    if anim.sprite_frames.has_animation("activado"):
+        anim.play("activado")
+    if t_feedback.time_left > 0.0:
+        t_feedback.stop()
+    t_feedback.start(seg)
+
+
+func _on_timer_timeout() -> void:
+    if not activo and anim.sprite_frames.has_animation("desactivado"):
+        anim.play("desactivado")
